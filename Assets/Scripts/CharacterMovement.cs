bda0bd8cfda21b1393de3cec8f8e11d01ae93e56
Ryan Giordano <rgiorda1@gmail.com>
2017-05-13 12:19:03 -0400

    Red spaces for current position

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -3,9 +3,9 @@
 public class CharacterMovement : MonoBehaviour {
 
     private Hex currentHex;
+    public float movementSpeed =10;
 
 	void Start () {
-
         // Once the map is generated, establish the currentHex.
         EventManager.Listen("Map Generated", (int x, int y) =>
         {
@@ -16,7 +16,8 @@ public class CharacterMovement : MonoBehaviour {
         // Listening for a hex to be clicked, taking the data passed with that event
         EventManager.Listen("Hex Clicked", (int x, int y) =>
         {
-
+            //
+            
             // finds the hex object at the given coordinates by creating the name from the coordinates
             GameObject selected = GameObject.Find("Hex_" + x + "_" + y);
 
@@ -25,10 +26,16 @@ public class CharacterMovement : MonoBehaviour {
             {
                 // set new current hex
                 // Set the player to the center of that selected hex
+                currentHex.GetComponent<Hex>().GetComponentInChildren<MeshRenderer>().material.color = Color.white;
                 currentHex = selected.GetComponent<Hex>();
                 Vector3 pos = selected.transform.position;
                 this.transform.position = new Vector3(pos.x, pos.y + 0.25f, pos.z);
+                selected.GetComponent<Hex>().GetComponentInChildren<MeshRenderer>().material.color = Color.red;
             }
         });
 	}
+    private void Update()
+    {
+
+    }
 }
